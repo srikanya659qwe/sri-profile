184011976232e833469868c601a7b23cdabd6d88
srikanya659qwe <srikanya659@gmail.com>
2020-04-28 12:10:38 +0530

    Updates

---
+++ precache-manifest.c54b0af2b0e42787f0e218b90bb2c1e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ec9ded279d7672935730d4c064f1fb",
+    "url": "/sri-profile/index.html"
+  },
+  {
+    "revision": "ca153037ca720209e504",
+    "url": "/sri-profile/static/css/main.4c222a32.chunk.css"
+  },
+  {
+    "revision": "288bd67b12658f69e22f",
+    "url": "/sri-profile/static/js/2.0fb5dc5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sri-profile/static/js/2.0fb5dc5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca153037ca720209e504",
+    "url": "/sri-profile/static/js/main.825744b4.chunk.js"
+  },
+  {
+    "revision": "501b96f3ac1d7503f99a",
+    "url": "/sri-profile/static/js/runtime-main.222a8c09.js"
+  }
+]);
